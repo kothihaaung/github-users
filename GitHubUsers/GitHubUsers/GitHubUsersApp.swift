bf03c6a132512973d47ad50b4fc5dfabd22160ae
Thiha Aung <leothihaaung@gmail.com>
2025-06-14 18:47:47 +0700

    project created

---
+++ GitHubUsers/GitHubUsers/GitHubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersApp.swift
+//  GitHubUsers
+//
+//  Created by Thiha the Dev on 2025/06/14.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
